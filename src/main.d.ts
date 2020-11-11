d4a5034123310092433bd5cf7b4d292c7c4c3a1a
betula <mail@betula.co>
2020-11-12 02:27:19 +0700

    fix: ts typings update

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,11 +1,14 @@
-export declare function box<T>(
+declare function box<T>(
   initialValue?: T,
   onChange?: (currentValue?: T, previousValue?: T) => void
 ): [() => T, (nextValue?: T) => void];
 
-export declare function sel<R>(body: () => R): [() => R, () => void];
+declare function sel<T extends () => any>(body: T): [T, () => void];
 
-export declare function expr<F extends (...args: any[]) => any>(
-  body: F,
-  updater?: () => void
-): [F, () => void];
+declare function expr<T extends () => any>(body: T): [T, () => void];
+declare function expr<T extends (...args: any[]) => any>(
+  body: T,
+  updater: () => void
+): [T, () => void];
+
+export { box, sel, expr };
